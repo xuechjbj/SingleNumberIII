461e76491b52a0b6ebb5dcc912f7b2a828e6477b
Chaojun Xue <xuechaojun88@gmail.com>
2016-01-07 09:27:31 -0800

    Initial Commit

---
+++ Algo-SingleIII/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algo-SingleIII
+//
+//  Created by Chaojun Xue on 1/7/16.
+//  Copyright © 2016 Chaojun Xue. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
